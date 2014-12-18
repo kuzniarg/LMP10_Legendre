b4d64cbebaab09b85eb71470743ca001495af3c8
kuzniarg <1niebian@gmail.com>
2014-12-18 12:52:02 +0100

    Poprawne rysowanie jednego wielomianu. Rozpoczęcie prac nad dodaniem kolejnych wielomianów

--- trunk/legendre.c
+++ trunk/legendre.c
@@ -9,17 +9,18 @@ void
 make_spl (points_t * pts, spline_t * spl)
 {
 	alloc_spl (spl, 1);
-	int i;
-	double ck, roznica = (pts->x[pts->n - 1] - pts->x[0]) / (pts->n - 1);
+	int i, n = pts->n;
+	double ck, roznica;
+	roznica = (pts->x[n - 1] - pts->x[0]) / (n - 1);
 	for (i = 0; i < 1; i++)
 	{
-		ck = (pts->y[pts->n - 1] * f(pts->x[pts->n-1])) / (f(pts->x[0]) * f(pts->x[0]));
+		ck = (pts->y[n - 1] * f(pts->x[n-1])) / (f(pts->x[0]) * f(pts->x[0]));
 
-		spl->x[i] = pts->x[0] + i * roznica;
-		spl->f[i] = ck* 1/2 * (3 * spl->x[i] * spl->x[i] - 1);
-		spl->f1[i] = ck* 3 * spl->x[i];
-		spl->f2[i] = ck*3;
-		spl->f3[i] = 0;
+		spl->x[i] = pts->x[0];
+		spl->f[i] = ck * (3 * spl->x[i] * spl->x[i] - 1)/6;
+		spl->f1[i] = ck * 3 * spl->x[i];
+		spl->f2[i] = ck * 3;
+		spl->f3[i] = 0; 
 	}
 	return;
 
