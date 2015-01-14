a112ce07d7e3ac59ef0db72ddeaa258fc4e9dcc9
kuzniarg <1niebian@gmail.com>
2015-01-14 14:43:04 +0100

    Działający moduł Legendre'a (możliwe najprostrze i najbardziej czytelne użycie aproksymacji średniokwadratowej), modyfikacja Makefile'a

--- trunk/apro_legendre.c
+++ trunk/apro_legendre.c
@@ -5,9 +5,6 @@
 #include <stdlib.h>
 #include <float.h>
 
-/* UWAGA: liczbę używanych f. bazowych można ustawić przez wartość
-          zmiennej środowiskowej APPROX_BASE_SIZE*/
-
 double P0 ()
 {
 	return 1;
@@ -31,61 +28,104 @@ double P3 (double x)
 void
 make_spl (points_t * pts, spline_t * spl)
 {
+	matrix_t *eqs = NULL;
+	double *x = pts->x;
+	double *y = pts->y;
+	int n = pts->n;
+	int i, j, k;
+	int nb = 4; 
+	double suma = 0;
 
-  matrix_t *eqs = NULL;
-  double *x = pts->x;
-  double *y = pts->y;
-  double a = x[0];
-  double b = x[pts->n - 1];
-  int i, j, k;
-  int nb = pts->n - 3 > 10 ? 10 : pts->n - 3;
-  char *nbEnv = getenv ("APPROX_BASE_SIZE");
-
-  if (nbEnv != NULL && atoi (nbEnv) > 0)
-    nb = atoi (nbEnv);
-
-  eqs = make_matrix (nb, nb + 1);
+	eqs = make_matrix (nb, nb + 1);
 
 
-  for (j = 0; j < nb; j++)
-    {
-      for (i = 0; i < nb; i++)
-	for (k = 0; k < pts->n; k++)
-	  add_to_entry_matrix (eqs, j, i,
-			       fi (a, b, nb, i, x[k]) * fi (a, b, nb, j,
-							    x[k]));
-
-      for (k = 0; k < pts->n; k++)
-	add_to_entry_matrix (eqs, j, nb, y[k] * fi (a, b, nb, j, x[k]));
-    }
-
-
-  if (piv_ge_solver (eqs))
-    {
-      spl->n = 0;
-      return;
-    }
-
-  if (alloc_spl (spl, nb) == 0)
-    {
-      for (i = 0; i < spl->n; i++)
+	for (j = 0; j < nb; j++)
 	{
-	  double xx = spl->x[i] = a + i * (b - a) / (spl->n - 1);
-	  xx += 10.0 * DBL_EPSILON;	// zabezpieczenie przed ulokowaniem punktu w poprzednim przedziale
-	  spl->f[i] = 0;
-	  spl->f1[i] = 0;
-	  spl->f2[i] = 0;
-	  spl->f3[i] = 0;
-	  for (k = 0; k < nb; k++)
-	    {
-	      double ck = get_entry_matrix (eqs, k, nb);
-	      spl->f[i] += ck * fi (a, b, nb, k, xx);
-	      spl->f1[i] += ck * dfi (a, b, nb, k, xx);
-	      spl->f2[i] += ck * d2fi (a, b, nb, k, xx);
-	      spl->f3[i] += ck * d3fi (a, b, nb, k, xx);
-	    }
+		for (i = 0; i < nb; i++)
+		{
+			suma = 0;
+			for (k = 0; k < pts->n; k++)
+			{
+				if (i==0)
+				{	
+					if (j==0)
+						suma += P0();
+					else if (j==1)
+						suma += P1(x[k]);
+					else if (j==2)
+						suma += P2(x[k]);
+					else if (j==3)
+						suma += P3(x[k]);
+				}
+				else if (i==1)
+				{
+					if (j==0)
+						suma += P1(x[k]);
+					if (j==1)
+						suma += 2 * P1(x[k]) * P1(x[k]);
+					if (j==2)
+						suma += 2 * P1(x[k]) * P2(x[k]);
+					if (j==3)
+						suma += 2 * P1(x[k]) * P3(x[k]);
+				}
+				else if (i==2)
+				{
+					if (j==0)
+						suma += P2(x[k]);
+					if (j==1)
+						suma += 2 * P2(x[k]) * P1(x[k]);
+					if (j==2)
+						suma += 2 * P2(x[k]) * P2(x[k]);
+					if (j==3)
+						suma += 2 * P2(x[k]) * P3(x[k]);
+				}
+				else if (i==3)
+				{
+					if (j==0)
+						suma += P3(x[k]);
+					if (j==1)
+						suma += 2 * P3(x[k]) * P1(x[k]);
+					if (j==2)
+						suma += 2 * P3(x[k]) * P2(x[k]);
+					if (j==3)
+						suma += 2 * P3(x[k]) * P3(x[k]);
+				}
+				
+			}
+			add_to_entry_matrix(eqs, j, i, suma);
+		}	
+		suma = 0;
+		for (k = 0; k < n; k++)
+		{
+			if (j==0)
+				suma += y[k];
+			if (j==1)
+				suma += 2 * y[k] * P1(x[k]);
+			if (j==2)
+				suma += 2 * y[k] * P2(x[k]);
+			if (j==3)
+				suma += 2 * y[k] * P3(x[k]);
+		}
+		add_to_entry_matrix(eqs, j, nb, suma);
 	}
-    }
 
 
+	if (piv_ge_solver (eqs))
+	{
+		spl->n = 0;
+		return;
+	}
+
+	if (alloc_spl (spl, 1) == 0)
+	{
+		double a0 = get_entry_matrix(eqs, 0, nb);
+		double a1 = get_entry_matrix(eqs, 1, nb);
+		double a2 = get_entry_matrix(eqs, 2, nb);
+		double a3 = get_entry_matrix(eqs, 3, nb);
+		double xx = spl->x[0] = pts->x[0];
+		spl->f[0] = 2*a0 + a1*P1(xx) + a2*P2(xx) + a3*P3(xx);
+		spl->f1[0] = 3 * ( xx * a2 + a3 * (5 * xx * xx - 1) / 2 );
+		spl->f2[0] = 3 * ( a2 + 5 * a3 * xx );
+		spl->f3[0] = 15 * a3;
+	}
 }
